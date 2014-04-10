a2e2caadf2acfd403ce567fc7ed816375d02958a
Lei Wang <lei.wang@yale.edu>
2014-04-10 16:17:52 -0400

    Monkey patched RISParser initializer.

--- lib/classes/parser.rb
+++ lib/classes/parser.rb
@@ -1,3 +1,14 @@
+class RefParsers::RISParser
+  def initialize
+    @type_key = "TY"
+    @types = %w(ABST ADVS ART BILL BOOK CASE CHAP COMP CONF CTLG DATA ELEC GEN HEAR ICOMM INPR JFULL JOUR MAP MGZN MPCT MUSIC NEWS PAMP PAT PCOMM RPRT SER SLIDE SOUND STAT THES UNBILl UNPB VIDEO WEB)
+    @terminator_key = "ER"
+    @line_regex = /^([A-Z][A-Z0-9])  -( (.*))?$/
+    @key_regex_order = 1
+    @value_regex_order = 3
+    @regex_match_length = 4
+  end
+end
 class RefParsers::LineParser
   def open(filename)
     references = []
